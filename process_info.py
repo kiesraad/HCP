5d3496bf463717a5b9b5e49cfa504de60c07a673
RubenW556 <ruben.Wijnbergen@live.nl>
2023-03-10 16:05:23 +0100

    changed  data order and column names

--- process_info.py
+++ process_info.py
@@ -3,11 +3,10 @@ def create_info(reporting_unit):
     total = reporting_unit.get("TotalCounted")
     blanco = reporting_unit.get("blanco")
     not_counted = reporting_unit.get("ongeldig")
-
-    result[0] = is_larger_than_3_percentage(total, blanco)
-    result[1] = is_larger_than_3_percentage(total, not_counted)
     if total == 0:
-        result[2] = True
+        result[0] = True
+    result[1] = is_larger_than_3_percentage(total, not_counted)
+    result[2] = is_larger_than_3_percentage(total, blanco)
 
     return result
 
