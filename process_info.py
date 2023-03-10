b5d06acd0dbce62d4496f43301aa1b156899d742
RubenW556 <ruben.Wijnbergen@live.nl>
2023-03-10 10:04:40 +0100

    Refactor process_info.py

--- process_info.py
+++ process_info.py
@@ -1,14 +1,16 @@
 def create_info(reporting_unit):
-    result = [False] * 3
+    result = [False] * 4
     total = reporting_unit.get("TotalCounted")
     blanco = reporting_unit.get("blanco")
     not_counted = reporting_unit.get("ongeldig")
 
-    if (int(blanco)*100/int(total)) >= 3:
-        result[0] = True
-    if (int(not_counted)*100/int(total)) >= 3:
-        result[1] = True
+    result[0] = is_larger_than_3_percentage(total, blanco)
+    result[1] = is_larger_than_3_percentage(total, not_counted)
     if total == 0:
         result[2] = True
 
     return result
+
+
+def is_larger_than_3_percentage(total, part):
+    return int(part)*100/int(total)
