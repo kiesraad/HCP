a560b1680ba2a8e99537ac7a80cf676673733cd1
RubenW556 <ruben.Wijnbergen@live.nl>
2023-03-13 12:47:05 +0100

    changed naming in main

--- main.py
+++ main.py
@@ -17,27 +17,27 @@ def main():
 
         # data on GSB niveau
         meta_data = XMLparser.get_meta_data(xml)
-        total_votes_path = ["Count", "Election", "Contests", "Contest", "TotalVotes"]
-        total_vote_structure = XMLparser.walk_xml_tree(xml, total_votes_path)
-        total_raw_info = XMLparser.get_vote_info(total_vote_structure)
+        total_vote_path = ["Count", "Election", "Contests", "Contest", "TotalVotes"]
+        total_vote_xml = XMLparser.walk_xml_tree(xml, total_vote_path)
+        total_raw_info = XMLparser.get_vote_info(total_vote_xml)
 
         # data on LSB niveau
         reporting_units = XMLparser.get_separate_entities_from_xml(xml)
-        raw_info = XMLparser.get_vote_info_reporting_unit_array(reporting_units)
+        reporting_units_info = XMLparser.get_vote_info_reporting_unit_array(reporting_units)
 
         # process info
         processed_info = {}
-        for key in raw_info.keys():
-            processed_info[key] = process_info.create_info(raw_info.get(key))
-
+        for key in reporting_units_info.keys():
+            processed_info[key] = process_info.create_info(reporting_units_info.get(key))
+        
         # output info
         print("----------------------------------------")
         pp.pprint(total_raw_info)
-        pp.pprint(raw_info)
+        pp.pprint(reporting_units_info)
         pp.pprint(processed_info)
         pp.pprint(meta_data)
 
-        csvWriter.write_csv(raw_info, processed_info, meta_data)
+        csvWriter.write_csv(reporting_units_info, processed_info, meta_data)
 
 
 if __name__ == '__main__':
