3db7a6f880c7128072931d4529c864ffbb9e4b0d
RubenW556 <ruben.Wijnbergen@live.nl>
2023-03-13 19:04:26 +0100

    changed afwijken to be written to a seperate file

--- csvWriter.py
+++ csvWriter.py
@@ -1,7 +1,7 @@
 import csv
 
 
-def write_csv(raw_info, processed_info, meta_data, afwijkeningen):
+def write_csv(raw_info, processed_info, meta_data):
     with open(meta_data.get("gebied")+"-"+meta_data.get("date")+'.csv', 'w', newline='') as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow(["sep=,"])
@@ -17,20 +17,43 @@ def write_csv(raw_info, processed_info, meta_data, afwijkeningen):
 
         writer.writerow([""])
         arr = ['stembureau', 'nr stembureau', 'stembureau met nul stemmen', 'stembureau >3% ongeldig',
-                         'stembureau >3 % blanco', 'stembureau met lijst > 50% afwijking', "afwijking per partij:"]
-        for key in afwijkeningen.keys():
-            for key2 in afwijkeningen[key].keys():
-                arr.append(key2)
-            break
+                'stembureau >3 % blanco', 'stembureau met lijst > 50% afwijking']
+
         writer.writerow(arr)
         for key in processed_info.keys():
             if check_any_true(processed_info[key]):
-                result_row = create_result_row(raw_info[key], processed_info[key], key.split("::SB")[1], afwijkeningen[key])
+                result_row = create_result_row(raw_info[key], processed_info[key], key.split("::SB")[1])
                 writer.writerow(result_row)
 
 
-def create_result_row(raw_info, info, nr, afwijkeningen):
-    write_row = [""]*(7 + len(afwijkeningen))
+def write_afwijkeningen(meta_data, afwijkingen, raw_info):
+    with open(meta_data.get("gebied")+"-"+meta_data.get("date")+'-afwijkingen.csv', 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(["sep=,"])
+        writer.writerow(["Versie controleprotocol"])
+        writer.writerow(["Beschrijving", "spreadsheet afwijkende percentages blanco en ongeldige stemmen, "
+                                         "stembureaus met nul stemmen en afwijkingen van het lijstgemiddelde > 50%"])
+        writer.writerow([""])
+        writer.writerow(["EML datum/tijd", meta_data["eml_date"]])
+        writer.writerow(["Verkiezing", meta_data["name"]])
+        writer.writerow(["Datum", meta_data["date"]])
+        writer.writerow(["Gebied", meta_data["gebied"]])
+        writer.writerow(["Nummer", meta_data["id"]])
+        arr = ["naam"]
+        for key in afwijkingen.keys():
+            for key2 in afwijkingen[key].keys():
+                arr.append(key2)
+            break
+        writer.writerow(arr)
+
+        for key in afwijkingen.keys():
+            arr = [raw_info[key]["name"]]
+            for key2 in afwijkingen[key].keys():
+                arr.append(int(afwijkingen[key][key2]))
+            writer.writerow(arr)
+
+def create_result_row(raw_info, info, nr):
+    write_row = [""]*6
     write_row[0] = raw_info["name"]
     write_row[1] = nr
     if info[0]:
@@ -42,11 +65,14 @@ def create_result_row(raw_info, info, nr, afwijkeningen):
     if info[3]:
         write_row[5] = info[3]
 
-    write_row[6] = ""
-    for idx, key in enumerate(afwijkeningen.keys()):
-        write_row[7+idx] = str(int(afwijkeningen[key]))+"%"
-
     return write_row
+
+
+def create_afwijking_row(afwijking):
+    return_arr = []
+    for idx, key in enumerate(afwijking.keys()):
+        return_arr[idx] = str(int(afwijking[key]))+"%"
+    return return_arr
 
 
 def check_any_true(info):
